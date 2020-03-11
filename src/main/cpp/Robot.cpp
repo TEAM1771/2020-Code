e9abee30c2870b59e000a7281f612c87fbc00577
rocketman1771 <meciviletto@gmail.com>
2020-03-11 14:01:52 -0400

    Started using PathWeaver

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -12,7 +12,19 @@ void Robot::AutonomousInit()
 }
 
 void Robot::EightBallAuton()
-{
+{ /*
+    #include <frc/Filesystem.h>
+    #include <frc/trajectory/TrajectoryUtil.h>
+    #include <wpi/Path.h>
+    #include <wpi/SmallString.h>
+
+    wpi::SmallString<64> deployDirectory;
+    frc::filesystem::GetDeployDirectory(deployDirectory);
+    wpi::sys::path::append(deployDirectory, "paths");
+    wpi::sys::path::append(deployDirectory, "SEG1.wpilib.json");
+
+    frc::Trajectory trajectory = frc::TrajectoryUtil::FromPathweaverJson(deployDirectory); */
+
     static bool doneDrivingForward = false;
     static bool doneDrivingBackward = false;
     static bool doneTurning = false;
